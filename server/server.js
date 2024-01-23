bade0723bb60fdd3e3cba2bd697099dc4a1d4ff5
Dylan Mayfield <dylanmayfield18@gmail.com>
2024-01-22 18:29:30 -0600

    debugged

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer } = require('@apollo/server');
-const expressMiddleware = require('@apollo/server/express4');
+// const expressMiddleware = require('@apollo/server/express4');
 // import ApolloServer
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
@@ -46,6 +46,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 });
 
 startApolloServer(typeDefs, resolvers);
